e3f546dfdc26e12a13131072d8a7458c60bfd8af
Achin Kumar <achin_kr@hotmail.com>
2018-06-04 16:55:11 +0530

    added js

---
+++ js.js
@@ -0,0 +1,29 @@
+
+$(document).ready(function () {
+    $('#btn').click(function (e) {
+      var dataString ={
+         username : $("#username").val(),
+         password : $("#password").val(),
+      }
+       
+        if (username == '' || password == '') {
+            alert("Please Fill All Fields");
+        } else {
+
+            $.ajax({
+                url: 'https://bc580197.ngrok.io/api/login',
+                type: 'POST',
+                data: 'dataString',
+                success: function () {
+                    alert('Submitted sucessfully');
+                },
+
+                error: function () {
+                    alert('Incorrect Username or Password');
+                },
+            });
+        }
+        e.preventDefault();
+    });
+});
+
